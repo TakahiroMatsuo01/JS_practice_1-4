d2d299e325eb30eb338484d99406b7a5bba49c50
TakahiroMatsuo01 <96337232+TakahiroMatsuo01@users.noreply.github.com>
2022-01-21 19:13:46 +0900

    課題修正後③

--- main.js
+++ main.js
@@ -1,23 +1,34 @@
 const getValue = () => {
     const title = '==============================\n現在持っているタスクの一覧\n==============================';
-    const addgenre_Array = ['掃除','買い物','散歩'];
-    const addtasks_Array = ['机を片付ける','牛乳を買う','散歩する'];
+    const tasks = [
+        {
+          title:'机を片付ける',
+          genre: '掃除'
+        },{
+            title:'牛乳を買う',
+            genre:'買い物',
+        },
+        {
+            title:'散歩する',
+            genre:'散歩'
+        }
+      ];
+
     let request = prompt('「確認、追加、削除、終了」のいずれかを入力してください。');
     let addtask = prompt('タスクを入力してください。');
     let addgenre = prompt('ジャンルを入力してください。');
              console.log(title);
-    addtasks_Array.forEach((value,index) => {
-                console.log(`${index}`,`[内容]:${value}`,`[ジャンル]${addgenre_Array[index]}`);
+    tasks.forEach((value,index) => {
+                console.log(`${index}`,`[内容]:${tasks[index].title}`,`[ジャンル]${tasks[index].genre}`);
 
     });   
     if(addtask && addgenre){
             console.log(title);
-            addtasks_Array.push(addtask);
-            addgenre_Array.push(addgenre)
+            tasks.title.push(addtask);
+            tasks.genre.push(addgenre);
             alert('新しいタスクを追加しました。');
-            addtasks_Array.forEach((value,index) => {
-                console.log(`${index}`,`[内容]:${value}`,`[ジャンル]${addgenre_Array[index]}`);
-
+            tasks.forEach((value,index) => {
+                console.log(`${index}`,`[内容]:${tasks[index].title}`,`[ジャンル]${tasks[index].genre}`);
     });
 　}   
 }
